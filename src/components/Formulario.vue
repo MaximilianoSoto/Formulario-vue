8393239a6408e073ee33f6dd29cc5cd2d6e04168
maxisoto <maxisoto@unap.cl>
2024-09-04 14:54:32 -0400

    primera version

---
+++ src/components/Formulario.vue
@@ -0,0 +1,109 @@
+<script setup>
+import {reactive, ref} from 'vue'
+
+  const paciente = reactive({
+    nombre: '',
+    propietario: '',
+    email:'',
+    alta: '',
+    sintomas: ''
+
+  })
+
+</script>
+
+<template>
+  <div class="md:w-1/2">
+    <h2 class="font-black text-3xl text-center"> Seguimiento Pacientes</h2>
+    <p class="text-lg mt-5 text-center mb-10">
+      Añade Pacientes y <span class="text-indigo-600 font-bold">Administralos</span>
+    </p>
+
+
+    <form class="bg-white shadow-md rounded-lg py-10 px-5" action="">
+      <div class="mb-5">
+        <label
+            for="mascota"
+            class="block text-gray-700 uppercase font-bold"
+        >
+          Nombre de mascota
+        </label>
+        <input
+            id="mascota"
+            type="text"
+            placeholder="Nombre de la mascota"
+            class="border-2 w-full p-2 mt-3 placeholder-indigo-400 rounded-md"
+            v-model="paciente.nombre"
+        >
+      </div>
+      <div class="mb-5">
+        <label
+            for="propietario"
+            class="block text-gray-700 uppercase font-bold"
+        >
+          Nombre del propietario
+        </label>
+        <input
+            id="propietario"
+            type="text"
+            placeholder="Nombre del propetario"
+            class="border-2 w-full p-2 mt-3 placeholder-indigo-400 rounded-md"
+            v-model="paciente.propietario"
+        >
+      </div>
+      <div class="mb-5">
+        <label
+            for="email"
+            class="block text-gray-700 uppercase font-bold"
+        >
+          Email
+        </label>
+        <input
+            id="email"
+            type="email"
+            placeholder="Email del propietario"
+            class="border-2 w-full p-2 mt-3 placeholder-indigo-400 rounded-md"
+            v-model="paciente.email"
+        >
+      </div>
+      <div class="mb-5">
+        <label
+            for="alta"
+            class="block text-gray-700 uppercase font-bold"
+        >
+          Alta
+        </label>
+        <input
+            id="alta"
+            type="date"
+            class="border-2 w-full p-2 mt-3 placeholder-indigo-400 rounded-md"
+            v-model="paciente.alta"
+        >
+      </div>
+      <div class="mb-5">
+        <label
+            for="sintomas"
+            class="block text-gray-700 uppercase font-bold"
+        >
+          Sintomas
+        </label>
+        <textarea
+            id="sintomas"
+            placeholder="Describe los síntomas"
+            type="date"
+            class="border-2 w-full p-2 mt-3 placeholder-indigo-400 rounded-md"
+            v-model="paciente.sintomas"
+        />
+      </div>
+
+      <input type="submit"
+             class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-400 cursor-pointer transition-colors"
+             value="Registrar paciente">
+    </form>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
