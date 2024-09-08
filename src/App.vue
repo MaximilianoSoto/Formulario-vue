0d959723e8f986bef72a7f09b7581da93339b368
MaximilianoSoto <maximiliano.soto.guardia99@gmail.com>
2024-09-07 20:26:46 -0400

    Tercer Commit

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,60 @@
 <script setup>
-
-import {reactive, ref} from 'vue'
+import { ref } from 'vue'
 import Header from './components/Header.vue'
 import Formulario from './components/Formulario.vue'
+import Paciente from './components/Paciente.vue'
 
+const pacientes = ref([])
 
-const pacientes = ref([1])
+const añadirPaciente = (nuevoPaciente) => {
+  pacientes.value.push(nuevoPaciente)
+}
 
-const paciente = reactive({
-  nombre: '',
-  propietario: '',
-  email:'',
-  alta: '',
-  sintomas: ''
+const eliminarPaciente = (id) => {
+  pacientes.value = pacientes.value.filter(paciente => paciente.id !== id)
+}
 
-})
+const editarPaciente = (pacienteActualizado) => {
 
+ 
+  const pacienteFiltrado = pacientes.value.filter(paciente => paciente.id === pacienteActualizado.id)[0]
 
+ 
 
+  
+  /*
+  pacientes.value = pacientes.value.map(paciente => 
+    paciente.id === pacienteActualizado.id ? pacienteActualizado : paciente
+  )*/
+}
 </script>
 
 <template>
   <div class="container mx-auto mt-20">
-    <Header></Header>
+    <Header />
 
     <div class="mt-12 md:flex">
-      <Formulario
-          v-model:nombre="paciente.nombre"
-          v-model:propietario="paciente.propietario"
-          v-model:email="paciente.email"
-          v-model:alta="paciente.alta"
-          v-model:sintomas="paciente.sintomas"
-
-      ></Formulario>
+      <Formulario @añadir-paciente="añadirPaciente" />
 
       <div class="md:w-1/2 md:h-screen overflow-y-scroll">
         <h3 class="font-black text-3xl text-center">Administra tus pacientes</h3>
 
         <div v-if="pacientes.length > 0">
-
+          <p class="text-lg mt-5 text-center mb-10">
+            Información de <span class="text-indigo-600 font-bold">Pacientes</span>
+          </p>
+          
+          <Paciente
+            v-for="paciente in pacientes"
+            :key="paciente.id"
+            :paciente="paciente"
+            @eliminar-paciente="eliminarPaciente"
+            @editar-paciente="editarPaciente"
+          />
         </div>
 
         <p v-else class="mt-20 text-2xl text-center">No Hay Pacientes</p>
-
       </div>
     </div>
   </div>
 </template>
-
-<style scoped>
-
-</style>
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,61 +1,51 @@
 <script setup>
-import {reactive, ref} from 'vue'
-import Alerta from "./Alerta.vue";
+import { reactive, ref } from 'vue'
+import Alerta from "./Alerta.vue"
+import { uid } from 'uid'
 
+const alerta = reactive({
+  tipo: '',
+  mensaje: ''
+})
 
-  const alerta = reactive({
-    tipo: '',
-    mensaje: ''
+const emit = defineEmits(['añadir-paciente'])
 
-  })
+const formulario = reactive({
+  nombre: '',
+  propietario: '',
+  email: '',
+  alta: '',
+  sintomas: ''
+})
 
-
-  defineEmits(['update:nombre','update:propietario','update:email','update:alta','update:sintomas'])
-
-
-  const props = defineProps({
-    nombre:{
-      type: String,
-      required: true
-    },
-    nombre:{
-      type: String,
-      required: true
-    },
-    email:{
-      type: String,
-      required: true
-    },
-    alta:{
-      type: String,
-      required: true
-    },
-    sintomas:{
-      type: String,
-      required: true
-    }
-
-
-  })
-
-  const validar = (e) => {
-
-    if(Object.values(props).includes('')){
-      alerta.mensaje = ' Todos los campos son obligatorios'
-      alerta.tipo = 'exito'
-      return
-    }
-
-    console.log('Todos los campos del formulario:', props)
-    alerta.mensaje = 'Formulario enviado correctamente'
-    alerta.tipo = 'exito'
+const validar = () => {
+  const camposVacios = Object.values(formulario).some(campo => campo === '');
+  
+  if (camposVacios) {
+    alerta.mensaje = 'Todos los campos son obligatorios';
+    alerta.tipo = 'error';
+    return;
   }
 
+  
+  emit('añadir-paciente', { ...formulario, id: uid() })
+  alerta.mensaje = 'Usuario agregado'
+  alerta.tipo = 'exito'
+
+  // Limpiar el formulario
+  Object.keys(formulario).forEach(key => formulario[key] = '')
+
+  setTimeout(()=> {
+    alerta.mensaje = ''
+    alerta.tipo = ''
+  },3000)
+ 
+}
 </script>
 
 <template>
   <div class="md:w-1/2">
-    <h2 class="font-black text-3xl text-center"> Seguimiento Pacientes</h2>
+    <h2 class="font-black text-3xl text-center">Seguimiento Pacientes</h2>
     <p class="text-lg mt-5 text-center mb-10">
       Añade Pacientes y <span class="text-indigo-600 font-bold">Administralos</span>
     </p>
@@ -65,97 +55,86 @@ import Alerta from "./Alerta.vue";
       :alerta="alerta"
     />
 
-
-    <form class="bg-white shadow-md rounded-lg py-10 px-5"  @submit.prevent="validar" >
+    <form class="bg-white shadow-md rounded-lg py-10 px-5" @submit.prevent="validar">
       <div class="mb-5">
         <label
-            for="mascota"
-            class="block text-gray-700 uppercase font-bold"
-            
+          for="mascota"
+          class="block text-gray-700 uppercase font-bold"
         >
           Nombre de mascota 
         </label>
         <input
-            id="mascota"
-            type="text"
-            placeholder="Nombre de la mascota"
-            class="border-2 w-full p-2 mt-3 placeholder-indigo-400 rounded-md"
-             @input="$emit('update:nombre', $event.target.value)"
+          id="mascota"
+          v-model="formulario.nombre"
+          type="text"
+          placeholder="Nombre de la mascota"
+          class="border-2 w-full p-2 mt-3 placeholder-indigo-400 rounded-md"
         >
-        
       </div>
       <div class="mb-5">
         <label
-            for="propietario"
-            class="block text-gray-700 uppercase font-bold"
+          for="propietario"
+          class="block text-gray-700 uppercase font-bold"
         >
           Nombre del propietario
         </label>
         <input
-            id="propietario"
-            type="text"
-            placeholder="Nombre del propetario"
-            class="border-2 w-full p-2 mt-3 placeholder-indigo-400 rounded-md"
-              @input="$emit('update:propietario', $event.target.value)"
-
+          id="propietario"
+          v-model="formulario.propietario"
+          type="text"
+          placeholder="Nombre del propietario"
+          class="border-2 w-full p-2 mt-3 placeholder-indigo-400 rounded-md"
         >
       </div>
       <div class="mb-5">
         <label
-            for="email"
-            class="block text-gray-700 uppercase font-bold"
+          for="email"
+          class="block text-gray-700 uppercase font-bold"
         >
           Email
         </label>
         <input
-            id="email"
-            type="email"
-            placeholder="Email del propietario"
-            class="border-2 w-full p-2 mt-3 placeholder-indigo-400 rounded-md"
-             @input="$emit('update:email', $event.target.value)"
-
+          id="email"
+          v-model="formulario.email"
+          type="email"
+          placeholder="Email del propietario"
+          class="border-2 w-full p-2 mt-3 placeholder-indigo-400 rounded-md"
         >
       </div>
       <div class="mb-5">
         <label
-            for="alta"
-            class="block text-gray-700 uppercase font-bold"
+          for="alta"
+          class="block text-gray-700 uppercase font-bold"
         >
           Alta
         </label>
         <input
-            id="alta"
-            type="date"
-            class="border-2 w-full p-2 mt-3 placeholder-indigo-400 rounded-md"
-            @input="$emit('update:alta', $event.target.value)"
-
+          id="alta"
+          v-model="formulario.alta"
+          type="date"
+          class="border-2 w-full p-2 mt-3 placeholder-indigo-400 rounded-md"
         >
       </div>
       <div class="mb-5">
         <label
-            for="sintomas"
-            class="block text-gray-700 uppercase font-bold"
+          for="sintomas"
+          class="block text-gray-700 uppercase font-bold"
         >
-          Sintomas
+          Síntomas
         </label>
         <textarea
-            id="sintomas"
-            placeholder="Describe los síntomas"
-            type="date"
-            class="border-2 w-full p-2 mt-3 placeholder-indigo-400 rounded-md"
-            @input="$emit('update:sintomas', $event.target.value)"
-
+          id="sintomas"
+          v-model="formulario.sintomas"
+          placeholder="Describe los síntomas"
+          class="border-2 w-full p-2 mt-3 placeholder-indigo-400 rounded-md"
         />
       </div>
 
-      <input type="submit"
-             class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-400 cursor-pointer transition-colors"
-             value="Registrar paciente">
+      <input 
+        type="submit"
+        class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-400 cursor-pointer transition-colors"
+        value="Registrar paciente"
+      >
     </form>
-
   </div>
 </template>
-
-<style scoped>
-
-</style>
